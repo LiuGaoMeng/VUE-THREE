58656de4d667646b18e8003d77746ed07c206df5
lgm <lgm123589@gmail.com>
2021-09-07 19:28:13 +0800

    111

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -4,7 +4,7 @@
  * @Author: liugm
  * @Date: 2021-09-03 09:59:39
  * @LastEditors: liugm
- * @LastEditTime: 2021-09-07 19:00:13
+ * @LastEditTime: 2021-09-07 19:20:54
 -->
 <template>
   <div id="viewDiv" class="viewDiv"></div>
@@ -109,8 +109,8 @@ export default {
           this.renderer = new THREE.WebGLRenderer();
           this.renderer.setSize(window.innerWidth, window.innerHeight);
           this.renderer.setClearColor("#FFFFFF");
-          this.renderer.setPixelRatio(window.devicePixelRatio);
-          this.renderer.setViewport(0, 0, sceneView.width, sceneView.height); //视图大小设置
+          // this.renderer.setPixelRatio(window.devicePixelRatio);
+          // this.renderer.setViewport(0, 0, sceneView.width, sceneView.height); //视图大小设置
           // document.getElementById("viewDiv").appendChild(this.renderer.domElement);
           this.renderer.autoClearDepth = false; // 定义renderer是否清除深度缓存
           this.renderer.autoClearStencil = false; // 定义renderer是否清除模板缓存
@@ -128,17 +128,18 @@ export default {
           toRenderCoordinates(sceneView, [108.3, 22.49, 10], 0, sceneView.spatialReference, resultPoint, 0, 1);
           debugger;
           //半球几何
-          var sphereGeom = new THREE.SphereGeometry(1000, 100, 100, 0, Math.PI * 2, 0, Math.PI / 2);
+          var sphereGeom = new THREE.SphereGeometry(100, 100, 100, 0, Math.PI * 2, 0, Math.PI / 2);
           //半球几何
           var material = new THREE.MeshBasicMaterial({
             transparent: true,
             opacity: 1,
-            color: new THREE.Color("red"),
+            color: new THREE.Color('#f28000'),
           });
           //使用转换结果resultPoint设置网格对象的位置
           var mesh = new THREE.Mesh(sphereGeom, material);
           mesh.position.set(resultPoint[0], resultPoint[1], resultPoint[2]);
           this.scene.add(mesh);
+          context.resetWebGLState();
         },
         render: (context) => {
           var cam = context.camera;
